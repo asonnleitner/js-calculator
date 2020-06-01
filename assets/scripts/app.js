d56e1da49025b4e8a00d1dccd661b17f9a11396c
asonnleitner <andreas@sonnleitner.cz>
2020-06-01 20:53:51 +0200

    basi leture complete + vue test

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 // Gets user input from input field
 function getUserNumberInput() {
@@ -11,32 +12,55 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     outputResult(currentResult, calcDescription);
 }
 
+function writeToLog(
+    operation,
+    prevResult,
+    operationNumber,
+    newResult
+) {
+    const logEntry = {
+        operation: operation,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
+    currentResult += enteredNumber;
     createAndWriteOutput('+', initialResult, enteredNumber);
+    writeToLog('+', initialResult, enteredNumber, currentResult)
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('-', initialResult, enteredNumber, currentResult)
+
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
-    createAndWriteOutput('*', initialResult, enteredNumber);
+    currentResult *= enteredNumber;
+    createAndWriteOutput('×', initialResult, enteredNumber);
+    writeToLog('×', initialResult, enteredNumber, currentResult)
+
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
-    createAndWriteOutput('/', initialResult, enteredNumber);
+    currentResult /= enteredNumber;
+    createAndWriteOutput('÷', initialResult, enteredNumber);
+    writeToLog('÷', initialResult, enteredNumber, currentResult)
+
 }
 
 addBtn.addEventListener('click', add);
